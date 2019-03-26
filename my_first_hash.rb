a80a3bfd8c505b1d70086aad4075249b10c170c1
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-26 05:09:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,8 +15,8 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
                   "porcelain vases" => 2,
+                  "whale bone corsets" => 5, 
                    "oil paintings" => 3
                  }
 
